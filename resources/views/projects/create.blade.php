fadd5ee5eb4b43900e0c6528736d08e545f397d8
Ronnie Selles <ronnie2nl@gmail.com>
2022-06-26 20:53:10 +0200

    404500

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -7,7 +7,7 @@
                 <h2>Add New Project</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+                <a class="btn btn-primary" href="{{ route('projects.indewerwerwex') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
             </div>
         </div>
     </div>
