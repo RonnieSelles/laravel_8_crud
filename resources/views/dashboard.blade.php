6e69c75ccd8216701079ef9b9b77206db2391dfe
Ronnie Selles <ronnie2nl@gmail.com>
2022-06-26 20:51:12 +0200

    404500

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,19 +4,19 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <divwerwerwer class="card">
-                    <div class="card-header">{{ __('Dashwwerwerboard') }}</div>
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
                     <div class="card-body">
                         @if (session('success'))
                             <div class="alert alert-success" role="alert">
-                                {{ session('suwerwerweccess') }}
+                                {{ session('success') }}
                             </div>
                         @endif
 
                         You are Logged In
                     </div>
-                </divwerwerwer>
+                </div>
             </div>
         </div>
     </div>
